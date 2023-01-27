55c2be971aab11da87668409775c0d3ed0053478
stephanieatte <122297064+stephanieatte@users.noreply.github.com>
2023-01-27 13:42:39 -0700

    Update play.sh

--- src/play.sh
+++ src/play.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-buildkite-agent meta-data set "exit_code" "BUILDKITE_COMMAND_EXIT_STATUS"
+buildkite-agent meta-data set "exit_code" "${BUILDKITE_COMMAND_EXIT_STATUS}"
 
-
-#steps_count=$(buildkite-agent meta-data get "steps-count")
